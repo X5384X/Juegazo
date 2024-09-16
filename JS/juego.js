9c0912860571174d2122b2c1cdb124098a07a9cd
Robin <105683782+rbncode@users.noreply.github.com>
2024-09-16 17:52:19 -0300

    Agrega pantalla y condiciones movimiento

--- JS/juego.js
+++ JS/juego.js
@@ -2,41 +2,56 @@
 
 //Constructor de los Jugadores
 class Character {
-    constructor(name, health, damage) {
-      //Atributos
-      this.name = name;
-      this.health = health;
-      this.maxhealth = health;
-      this.damage = damage;
-    }
-    //Verifica si el personaje esta vivo
-    isAlive() {
-      return this.health > 0;
-    }
-  
-    //Ataca a otro personaje seleccionado
-    attack(target) {
-      console.log(`${this.name} inflige ${this.damage} de daño a ${target.name}`);
-      target.health -= this.damage;
-    }
-  
-    //Retorna la información actual del personaje
-    status() {
-      return `${this.name} - HP ${this.health}/${this.maxhealth}`;
-    }
+  constructor(name, health, damage) {
+    //Atributos
+    this.name = name;
+    this.health = health;
+    this.maxhealth = health;
+    this.damage = damage;
+  }
+  //Verifica si el personaje esta vivo
+  isAlive() {
+    return this.health > 0;
+  }
 
-    getName(){
-        return `${this.name}`;
-    }
+  //Ataca a otro personaje seleccionado
+  attack(target) {
+    console.log(`${this.name} inflige ${this.damage} de daño a ${target.name}`);
+    target.health -= this.damage;
+  }
 
-    getHealth(){
-        return `${this.health}`;
-    }
+  //Retorna la información actual del personaje
+  status() {
+    return `${this.name} - HP ${this.health}/${this.maxhealth}`;
+  }
+
+  getName() {
+    return `${this.name}`;
+  }
+
+  getHealth() {
+    return `${this.health}`;
+  }
 }
 
 //Movimiento -- Por Hacer
 
+const pantalla = document.getElementById("pantallaJuego");
+const pantallaxt = pantalla.getContext("pantalla");
 
+//Movimiento Player 1
+
+let player1Up = false;
+let player1Left = false;
+let player1Right = false;
+let player1Down = false;
+
+//Movimiento Player 2
+
+let player2Up = false;
+let player2Left = false;
+let player2Right = false;
+let player2Down = false;
 
 //Fisicas Bala -- Por Hacer
 
@@ -48,4 +63,3 @@ console.log(player2);
 
 document.getElementById("nombre-p1").innerHTML = player1.getName();
 document.getElementById("nombre-p2").innerHTML = player2.getName();
-
